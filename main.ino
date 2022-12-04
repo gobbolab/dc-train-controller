438a8ab96ff3a42d99c71db1ce4f7c35f9e58d4e
James Hogle <jhogle117@gmail.com>
2022-12-03 21:04:53 -0600

    add pot based throttle control

--- main.ino
+++ main.ino
@@ -6,9 +6,13 @@
 #define CHANNEL_A_BRAKE_PIN 9
 #define CHANNEL_A_PIN 3
 
+#define SPEED_PIN A0
+
 #define AUTO_SPEED 200
 
 void setup() {
+    Serial.begin(9600);
+
     pinMode(POWER_LED_PIN, OUTPUT);
     pinMode(AUTO_LED_PIN, OUTPUT);
     pinMode(AUTO_PIN, INPUT_PULLUP);
@@ -35,8 +39,13 @@ void AutoMode() {
 }
 
 void ManualMode() {
+    int rawSpeed = analogRead(SPEED_PIN);
+    int speed = map(rawSpeed, 0, 1023, 0, 255);
+    Serial.println(speed);
+
     digitalWrite(AUTO_LED_PIN, LOW);
-    analogWrite(CHANNEL_A_PIN, 0);
+    analogWrite(CHANNEL_A_PIN, speed);
+    delay(100);
 }
 
 bool IsAutoMode() {
